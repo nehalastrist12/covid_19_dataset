310364962ebd6e58eff0138d6a2b7046ab91b6b2
Nehal Kashyap <nehalkashyap17@gmail.com>
2025-09-16 23:59:32 +0530

    covid deaths

---
+++ covid.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+df = pd.read_csv('worldometer_data.csv')
+
+country_deaths = df.groupby("Country/Region")["TotalDeaths"].sum().reset_index()
+
+fig = px.choropleth(country_deaths, 
+                    locations="Country/Region", 
+                    locationmode="country names",
+                    color="TotalDeaths",
+                    title="COVID-19 Deaths by Country",
+                    color_continuous_scale="Reds")
+fig.show()
+fig.write_html("covid_deaths_map.html")
